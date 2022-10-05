2223bc50337056d16e56228a2320340281160b5d
JimmyGx6 <jaime92_92@hotmail.com>
2022-10-05 20:18:19 +0200

    Iteration #1

---
+++ Functions/Máximo.js
@@ -0,0 +1,11 @@
+// Completa la función que tomando dos números como argumento devuelva el más alto.
+function sum(numberOne , numberTwo) {
+    if(numberOne > numberTwo){
+        console.log(numberOne);
+    } else {
+    console.log(numberTwo);
+    }
+  }
+
+sum(7 , 5);
+sum(15 , 22);
