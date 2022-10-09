7ad216e10abb5d880534eb068bad4ea1c7adf8d3
JimmyGx6 <jaime92_92@hotmail.com>
2022-10-09 10:18:08 +0200

    FIX: loop adjust

--- Loops/for.js
+++ Loops/for.js
@@ -4,10 +4,10 @@
 
 const placesToTravel = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
 
-for (let i = 0; i < placesToTravel.length; i++){
+for (i = 0; i < placesToTravel.length; i++){
     destination = placesToTravel[i];
 
-    destination.id === 11 || destination.id === 40 ? placesToTravel.splice(i, 1) : destination;
+    destination.id === 11 || destination.id === 40 ? placesToTravel.splice(i, 1) && i-- : destination;
 }
 
 console.log(placesToTravel);
